432d3495442a0425b49252b7da0b074229efbed7
hoangsonld <ledinhhoangson316@gmail.com>
2018-07-11 16:34:21 +0700

    instantly incrementing num of likes, instantly changing from unlike-> like ajax

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,11 @@
 module PostsHelper
+
+  def display_likes(post)
+    votes = post.votes_for.up.by_type(User)
+    return list_likers(voters) if votes.size <= 8
+    count_likers(votes)
+  end
+
   def likers_of(post)
     votes = post.votes_for.up.by_type(User)
     user_names = []
@@ -16,6 +23,16 @@ module PostsHelper
   end
 
   private
+
+  def list_likers(votes)
+    user_names = []
+    unless votes.blank?
+      votes.voters.each do |voter|
+        user_names.push(link_to voter.user_name, profile_path(voter.user_name), class: 'user-name')
+      end
+      user_names.to_sentence.html_safe + like_plural(votes)
+    end
+  end
 
   def like_plural(votes)
     return ' like this' if votes.count > 1
